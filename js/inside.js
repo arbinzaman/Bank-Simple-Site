733d781ecb024c533b6732ca4aa4e56e9d3a29db
arbinzaman <arbinzmn@gmail.com>
2022-08-20 15:07:42 +0600

    fixed some thngs

--- js/inside.js
+++ js/inside.js
@@ -3,12 +3,25 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
     const valueDepositeString = deposite.value;
     const valueDeposite =parseFloat(valueDepositeString); 
   
+
+    if(isNaN(deposite)){
+      alert(('Please Provide a number'))
+     }
+     deposite.value='';
+  
     const currentTotal = document.getElementById('deposit-total');
     const previousCurrentTotalString = currentTotal.innerText;     
     const previousCurrent = parseFloat(previousCurrentTotalString);
 
     const newCurrentTotal = valueDeposite + previousCurrent;
     currentTotal.innerText = newCurrentTotal;
+      // added with Total
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalElementString = balanceTotal.innerText;
+    const balanceTotalElement = parseFloat(balanceTotalElementString);
+    const wholeTotal = balanceTotalElement + newCurrentTotal;
+    balanceTotal.innerText = wholeTotal;
+
 
   })
   
@@ -21,21 +34,40 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
    const withdrawString   = valueWithdraw.value;
    const withdrawTotal = parseFloat(withdrawString);
 
+   withdrawTotal.value= '';
+   if(isNaN(withdrawTotal)){
+    alert('Please Provide a number')
+    
+    return;
+   }
+ 
+
    const previousWithdraw = document.getElementById('withdraw-total');
    const previousWithdrawTotalString = previousWithdraw.innerText;
    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-
+   
+   
+  
+  
+  
+ 
    const newWithdrawTotal = withdrawTotal + previousWithdrawTotal ;
    previousWithdraw.innerText= newWithdrawTotal;
+   
 
-
+  // minus with Total
    const balanceTotal = document.getElementById('balance-total');
    const balanceTotalElement = balanceTotal.innerText;
    const wholeTotal = balanceTotalElement -newWithdrawTotal;
    balanceTotal.innerText = wholeTotal;
 
-   
+   if( valueWithdraw>balanceTotalElement){
+    alert('Baper bank e eto tk nai')
+  
+   }
+   balanceTotalElement.value='';
 })
+
 
 
 
